1b94b5eafed2747d91019a04f68e4baa106c18ce
Petar787 <petrov.m.petar.787@gmail.com>
2025-02-04 20:25:18 +0200

    add create movie
    add uuid

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,6 @@
 export default [
     {
-        id: '1',
+        id: 'a422bffb-edb7-40cf-b0de-8dba25978a99',
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
@@ -11,7 +11,7 @@ export default [
         category: 'Movie'
     },
     {
-        id: '2',
+        id: 'ag52bffb-edb7-40cf-b0de-8dba25978a77',
         title: 'Man of Steel',
         genre: 'Superhero',
         description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
@@ -20,5 +20,16 @@ export default [
         year:'2013',
         rating: 7.1,
         category: 'Movie'
-    }
+    },
+    {
+        id: 'bf22bffb-edb7-40cf-b0de-8dba25978a35',
+        title: 'Avengers: Endgame',
+        category: 'movie',
+        genre: 'Superhero',
+        director: ' Anthony Russo Joe Russo',
+        year: '2019',
+        imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdNZ-1hREQVcizp7gB7hKFvHajOpT9sMtudQ&s',
+        rating: '8.4',
+        description: "After the devastating events of Отмъстителите: Война без край (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos'"
+      }
 ]
