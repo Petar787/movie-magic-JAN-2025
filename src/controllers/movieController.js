663ecc9c4aaf3a9aaf0545c61ea100db5382ddf8
Petar787 <petrov.m.petar.787@gmail.com>
2025-02-04 18:32:22 +0200

    add details page with hardcoded data

---
+++ src/controllers/movieController.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+
+const movieController = Router();
+
+movieController.get('/create', (req, res) => {
+    res.render('create');
+});
+
+movieController.get('/:movieId/details', (req, res) => {
+    res.render('details');
+});
+
+export default movieController;
