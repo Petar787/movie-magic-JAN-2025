c7e4d8c3c172e73c16f0214b87fc8e79ea44b864
Petar787 <petrov.m.petar.787@gmail.com>
2025-02-04 19:49:52 +0200

    parse form data on server

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -9,11 +9,17 @@ movieController.get('/create', (req, res) => {
     res.render('create');
 });
 
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    res.end();
+})
+
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
 
     const movie = movieService.findMovie(movieId)
-    res.render('details');
+    res.render('details', {movie});
 });
 
 export default movieController;
